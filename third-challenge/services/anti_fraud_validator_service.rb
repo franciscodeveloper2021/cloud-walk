e4912e1cd6a60d3e7b5d4bd3dab541e4066a8077
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-07-07 03:17:19 -0300

    (add) test cases for conditions

--- third-challenge/services/anti_fraud_validator_service.rb
+++ third-challenge/services/anti_fraud_validator_service.rb
@@ -1,6 +1,43 @@
+require 'date'
+
 class AntiFraudValidatorService
+  attr_reader :transaction_payload
 
   def initialize(transaction_payload:)
     @transaction_payload = transaction_payload
+  end
+
+  def call
+    if is_any_condition_to_deny_true?
+      TransactionResponseDto.new(transaction_payload["transaction_id"], "deny")
+    else
+      TransactionResponseDto.new(transaction_payload["transaction_id"], "allow")
+    end
+  end
+
+  private
+
+  def is_any_condition_to_deny_true?
+    conditions.any? { |condition| send(condition) }
+  end
+
+  def conditions
+    [
+      :is_high_transaction_happening_at_midnight?,
+      :is_device_id_nil?
+    ]
+  end
+
+  def is_high_transaction_happening_at_midnight?
+    transaction_date = DateTime.parse(transaction_payload["transaction_date"])
+    transaction_date.hour == 0 && is_transaction_amount_too_high
+  end
+
+  def is_device_id_nil?
+    transaction_payload["device_id"].nil?
+  end
+
+  def is_transaction_amount_too_high
+    transaction_payload["transaction_amount"] > 2000
   end
 end
--- third-challenge/spec/anti_fraud_validator_service_spec.rb
+++ third-challenge/spec/anti_fraud_validator_service_spec.rb
@@ -1,6 +1,6 @@
 require 'rspec'
 require_relative '../services/anti_fraud_validator_service'
-
+require_relative '../dtos/transaction_response_dto'
 
 RSpec.describe AntiFraudValidatorService do
   let(:transaction_payload) do
@@ -9,16 +9,44 @@ RSpec.describe AntiFraudValidatorService do
       "merchant_id" => 29744,
       "user_id" => 97051,
       "card_number" => "434505******9116",
-      "transaction_date" => "2019-11-31T23:16:32.812632",
+      "transaction_date" => "2019-11-30T23:16:32.812632",
       "transaction_amount" => 373,
       "device_id" => 285475
     }
   end
   let(:service) { described_class.new(transaction_payload: transaction_payload) }
 
+  let(:transaction_response_dto_deny) { TransactionResponseDto.new(transaction_payload["transaction_id"], "deny") }
+  let(:transaction_response_dto_allow) { TransactionResponseDto.new(transaction_payload["transaction_id"], "allow") }
+
   describe "#initialize" do
     it "initializes with transaction_payload as instance variable" do
       expect(service.instance_variable_get(:@transaction_payload)).to eq(transaction_payload)
+    end
+  end
+
+  describe "#call" do
+    context "with suspicious activity" do
+      context "when high transaction is happening at midnight" do
+        it "returns recommendation deny" do
+          transaction_payload["transaction_date"] = "2019-11-30T00:00:00.000000"
+          transaction_payload["transaction_amount"] = 2200
+
+          result = service.call
+
+          expect(result).to eq(transaction_response_dto_deny)
+        end
+      end
+
+      context "when device id is not present" do
+        it "returns recommendation deny" do
+          transaction_payload["device_id"] = nil
+
+          result = service.call
+
+          expect(result).to eq(transaction_response_dto_deny)
+        end
+      end
     end
   end
 end
