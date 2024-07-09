65e318bfb1db96538239e6e1d3067dfaee17f6ea
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-07-08 21:45:49 -0300

    finishing rules implementation

--- third-challenge/spec/anti_fraud_validator_service_spec.rb
+++ third-challenge/spec/anti_fraud_validator_service_spec.rb
@@ -38,6 +38,43 @@ RSpec.describe AntiFraudValidatorService do
 
   describe "#call" do
     context "with suspicious activity" do
+      context "when any transaction on transaction history receives a charge back" do
+        it "returns recommendation deny" do
+          transactions_history << {
+            "transaction_id" => 2342356,
+            "merchant_id" => 29744,
+            "user_id" => 97051,
+            "card_number" => "434505******9116",
+            "transaction_date" => "2019-11-30T23:12:32.812632",
+            "transaction_amount" => 100,
+            "device_id" => 285475,
+            "has_charge_back" => true
+          }
+
+          result = service.call
+
+          expect(result).to eq(transaction_response_dto_deny)
+        end
+      end
+  
+      context "when user is trying too many transactions in a row" do
+        it "returns recommendation deny" do
+          transactions_history << {
+            "transaction_id" => 2342356,
+            "merchant_id" => 29744,
+            "user_id" => 97051,
+            "card_number" => "434505******9116",
+            "transaction_date" => (DateTime.parse(transaction_payload["transaction_date"]) - Rational(4, 24 * 60)).to_s,
+            "transaction_amount" => 100,
+            "device_id" => 285475
+          }
+
+          result = service.call
+
+          expect(result).to eq(transaction_response_dto_deny)
+        end
+      end
+
       context "when transaction amount exceeds the limit for transaction" do
         it "returns recommendation deny" do
           transaction_payload["transaction_amount"] = described_class::LIMIT_AMOUNT_FOR_TRANSACTION + 1
@@ -62,43 +99,6 @@ RSpec.describe AntiFraudValidatorService do
       context "when device id is nil" do
         it "returns recommendation deny" do
           transaction_payload["device_id"] = nil
-
-          result = service.call
-
-          expect(result).to eq(transaction_response_dto_deny)
-        end
-      end
-
-      context "when user is trying too many transactions in a row" do
-        it "returns recommendation deny" do
-          transactions_history << {
-            "transaction_id" => 2342356,
-            "merchant_id" => 29744,
-            "user_id" => 97051,
-            "card_number" => "434505******9116",
-            "transaction_date" => (DateTime.parse(transaction_payload["transaction_date"]) - Rational(4, 24 * 60)).to_s,
-            "transaction_amount" => 100,
-            "device_id" => 285475
-          }
-
-          result = service.call
-
-          expect(result).to eq(transaction_response_dto_deny)
-        end
-      end
-
-      context "when any transaction on transaction history receives a charge back" do
-        it "returns recommendation deny" do
-          transactions_history << {
-            "transaction_id" => 2342356,
-            "merchant_id" => 29744,
-            "user_id" => 97051,
-            "card_number" => "434505******9116",
-            "transaction_date" => "2019-11-30T23:12:32.812632",
-            "transaction_amount" => 100,
-            "device_id" => 285475,
-            "has_charge_back" => true
-          }
 
           result = service.call
 
